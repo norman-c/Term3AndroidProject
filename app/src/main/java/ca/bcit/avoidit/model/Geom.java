db9486172f55ec126bfea67055b8ef2eeda56891
norman-c <52259325+norman-c@users.noreply.github.com>
2020-02-25 11:37:10 -0800

    models

--- app/src/main/java/ca/bcit/avoidit/model/Geom.java
+++ app/src/main/java/ca/bcit/avoidit/model/Geom.java
@@ -13,5 +13,19 @@ public class Geom {
     @Expose
     private Coordinates coordinates;
 
+    public String getType() {
+        return type;
+    }
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
 }
--- app/src/main/java/ca/bcit/avoidit/model/Records.java
+++ app/src/main/java/ca/bcit/avoidit/model/Records.java
@@ -20,4 +20,36 @@ public class Records {
     @SerializedName("facet_groups")
     @Expose
     private String facet_groups;
+
+    public int getNhits() {
+        return nhits;
+    }
+
+    public void setNhits(int nhits) {
+        this.nhits = nhits;
+    }
+
+    public String[] getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(String[] parameters) {
+        this.parameters = parameters;
+    }
+
+    public Records getRecords() {
+        return records;
+    }
+
+    public void setRecords(Records records) {
+        this.records = records;
+    }
+
+    public String getFacet_groups() {
+        return facet_groups;
+    }
+
+    public void setFacet_groups(String facet_groups) {
+        this.facet_groups = facet_groups;
+    }
 }
