256add8141a4cb9327df91483572a0820b437270
kihyunchoi <gmyfriend78@gmail.com>
2020-02-25 11:32:08 -0800

    create model Fields

--- app/src/main/java/ca/bcit/avoidit/model/Fields.java
+++ app/src/main/java/ca/bcit/avoidit/model/Fields.java
@@ -1,4 +1,67 @@
 package ca.bcit.avoidit.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Fields {
+
+    @SerializedName("project")
+    @Expose
+    private String project;
+
+    @SerializedName("geom")
+    @Expose
+    private Geom geom;
+
+    @SerializedName("location")
+    @Expose
+    private String location;
+
+    @SerializedName("url_link")
+    @Expose
+    private String url_link;
+
+    @SerializedName("comp_date")
+    @Expose
+    private String comp_date;
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public Geom getGeom() {
+        return geom;
+    }
+
+    public void setGeom(Geom geom) {
+        this.geom = geom;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getUrl_link() {
+        return url_link;
+    }
+
+    public void setUrl_link(String url_link) {
+        this.url_link = url_link;
+    }
+
+    public String getComp_date() {
+        return comp_date;
+    }
+
+    public void setComp_date(String comp_date) {
+        this.comp_date = comp_date;
+    }
 }
