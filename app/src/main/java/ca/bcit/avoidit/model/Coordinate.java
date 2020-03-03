224316c4c30ef55a6c96c95a99338dc965e71d3a
Paolo Del Rio <pdelrio@csdr.ca>
2020-03-03 10:45:50 -0800

    Merge branch 'master' of D:\AndroidProjects\Term3AndroidProject with conflicts.

---
+++ app/src/main/java/ca/bcit/avoidit/model/Coordinate.java
@@ -0,0 +1,28 @@
+package ca.bcit.avoidit.model;
+
+import java.util.ArrayList;
+
+public class Coordinate {
+
+    private double latitude;
+
+    private double longitude;
+
+    private double[] latLng;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
